8871dbe9fa13da133bc8cbd842f5f6d1bf701906
Jaim Grymauch <kaelin.grymauch@gmail.com>
2018-11-15 11:08:36 +0100

    bugfix : Revue de code Case.

--- src/com/monopoly/CaseAchetable.java
+++ src/com/monopoly/CaseAchetable.java
@@ -4,45 +4,48 @@ package com.monopoly;
 * ainsi que les méthodes permettant de modéliser les comportements de ces cases
 */
 
-public class CaseAchetable extends Case {
-	
+public abstract class CaseAchetable extends Case {
+
 	private int prix;
-	private Joueur proprietaire; 
-	
-	public CaseAchetable (String nom){
-		super.(nom);
-	}	
-	
-	public int getPrix() {
-        return this.prix;
-    }
-	
-	public void setPrix(int n_prix) {
-        this.prix = n_prix;
-    }
-	
-	public int getProprietaire() {
-        return this.proprietaire;
-    }
-	
-	public void setProprietaire(Joueur n_proprietaire) {
-        this.proprietaire = n_proprietaire;
-    }
+	private Joueur proprietaire;
+
+	public CaseAchetable(String nom) {
+		super(nom);
+	}
+
 	/**
-	* Methode qui permet de représenter le loyer qu'un joueur doit payer au proprio (si il y en a un)
-	*
-	*/
-	
+	 * Methode qui permet de représenter le loyer qu'un joueur doit payer au proprio (si il y en a un)
+	 */
 	public abstract int loyer();
-	
-	/** 
-	* Methode qui permet de définir un propriétaire si il n'existe pas encore
-	* @param investisseur personne qui veut acheter la case
-	*/
-	public void acheter(Joueur investisseur){
-		if (this.proprietaire == null){
-			setProprietaire(Joueur);
-		} else{
+
+	/**
+	 * Methode qui permet de définir un propriétaire si il n'existe pas encore
+	 *
+	 * @param investisseur personne qui veut acheter la case
+	 */
+	public void acheter(Joueur investisseur) {
+		if (this.proprietaire == null) {
+			setProprietaire(investisseur);
+		} else {
 			System.out.println("Il y a déjà un propriétaire");
-	}	
+		}
+	}
+
+	public int getPrix() {
+		return prix;
+	}
+
+	public Joueur getProprietaire() {
+		return proprietaire;
+	}
+
+
+	public void setPrix(int prix) {
+		this.prix = prix;
+	}
+
+	public void setProprietaire(Joueur proprietaire) {
+		this.proprietaire = proprietaire;
+	}
+
 }
