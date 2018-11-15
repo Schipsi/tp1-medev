a44ad9dc97aef351328eac39ce08bf9c61439724
Ismail <Ismail@wpad.ec-nantes.fr>
2018-11-15 10:33:38 +0100

    1st commit after configuration

--- src/com/monopoly/Gare.java
+++ src/com/monopoly/Gare.java
@@ -1,5 +1,33 @@
 package com.monopoly;
 
+/**
+ *
+ * @author Ismail
+ */
 public class Gare extends CaseAchetable {
     
+    /**
+     *
+     */
+    public Gare () {
+        
+    }
+    
+    /**
+     *
+     * @return
+     */
+    public int loyer() {
+       if this.proprio =  
+        
+    }
+    
+    /**
+     *
+     * @param J Le joueur qui passe par la gare
+     * @return
+     */
+    public Joueur acheter(Joueur J) {
+       
+    }
 }
