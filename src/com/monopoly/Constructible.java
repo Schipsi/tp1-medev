eb1769b2b76e5684878443b904c330e0d18a3eb5
Adil Erchouk <adil.erchouk@gmail.com>
2018-11-15 09:39:46 +0100

    Fetch (Omid) : Ajout des constucteurs dans Constructible.java

--- src/com/monopoly/Constructible.java
+++ src/com/monopoly/Constructible.java
@@ -20,6 +20,38 @@ public class Constructible extends CaseAchetable {
      * Cet attribut représente la possibilité de construire un (unique) hôtel sur la case
      */
     public boolean hotel;
+    
+    
+
+    /**
+     * Ce constructeur permet d'instancier une Case Constructible par défaut ne contenant pas de maison ou d'hôtel
+     */    
+    public Constructible(){
+        
+        super();
+        this.maison = 0;
+        this.hotel = false;
+        
+        
+    }
+    
+    
+    /**
+     * Ce constructeur permet d'instancier une Case Constructible en précisant la valeur de chacun de ses attributs
+     * @param nom le nom de la case
+     * @param prix le prix de la case
+     * @param proprio le propriétaire de la case
+     * @param maison le nombre de maison sur la case
+     * @param hotel la possibilité de construire un hôtel ou non
+     */
+    public Constructible(String nom, int prix, Joueur proprio, int maison, boolean hotel){
+        
+        super(nom, prix, proprio);
+        this.maison = maison;
+        this.hotel = hotel;
+        
+    }
+    
 
     public int getMaison() {
         return maison;
