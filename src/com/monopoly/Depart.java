852a5835b7af3640d05b9517e0459dfdf31ca549
oaitrhol <45061352+oaitrhol@users.noreply.github.com>
2018-11-15 09:34:20 +0100

    Add files via upload

--- src/com/monopoly/Depart.java
+++ src/com/monopoly/Depart.java
@@ -1,4 +1,18 @@
-package com.monopoly;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp1.medev;
 
-public class Depart {
+/**
+ *
+ * @author ouissal ait rholofo
+ */
+public class Depart extends CaseNonAchetable {
+    public Depart() {
+        super();
+
+}
+    
 }
