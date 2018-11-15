4bd830f3ab9da1ebabad0743e60f4f00333a0379
WoWy\Andrianirina <andrianirina.rakotoharisoa@eleves.ec-nantes.fr>
2018-11-15 11:56:53 +0100

    Ajout toString

---
+++ src/com/monopoly/Case.java
@@ -0,0 +1,26 @@
+package com.monopoly;
+/**
+ *Classe abstraite qui permet de modéliser une case
+ *
+ */
+public abstract class Case {
+
+    /**
+     * nom indique le nom de la case
+     */
+    private String nom;
+
+    public Case(String nom){
+        this.nom = nom;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+    
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+	
+	public abstract String toString();
+}
---
+++ src/com/monopoly/CaseAchetable.java
@@ -0,0 +1,51 @@
+package com.monopoly;
+/**
+*Classe qui permet de définir les caractéristiques des cases achetables (type de case, prix, propriétaires)
+* ainsi que les méthodes permettant de modéliser les comportements de ces cases
+*/
+
+public abstract class CaseAchetable extends Case {
+
+	private int prix;
+	private Joueur proprietaire;
+
+	public CaseAchetable(String nom) {
+		super(nom);
+	}
+
+	/**
+	 * Methode qui permet de représenter le loyer qu'un joueur doit payer au proprio (si il y en a un)
+	 */
+	public abstract int loyer();
+
+	/**
+	 * Methode qui permet de définir un propriétaire si il n'existe pas encore
+	 *
+	 * @param investisseur personne qui veut acheter la case
+	 */
+	public void acheter(Joueur investisseur) {
+		if (this.proprietaire == null) {
+			setProprietaire(investisseur);
+		} else {
+			System.out.println("Il y a déjà un propriétaire");
+		}
+	}
+
+	public int getPrix() {
+		return prix;
+	}
+
+	public Joueur getProprietaire() {
+		return proprietaire;
+	}
+
+
+	public void setPrix(int prix) {
+		this.prix = prix;
+	}
+
+	public void setProprietaire(Joueur proprietaire) {
+		this.proprietaire = proprietaire;
+	}
+
+}
