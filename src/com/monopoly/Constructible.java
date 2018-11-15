8aacb0fea600099ebddad5e619b0d987167d1586
Adil Erchouk <adil.erchouk@gmail.com>
2018-11-15 09:24:57 +0100

    Feature (Omid) : Implémentation de Constructible.java

--- src/com/monopoly/Constructible.java
+++ src/com/monopoly/Constructible.java
@@ -1,4 +1,40 @@
-package com.monopoly;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package monopoly;
 
-public class Constructible {
+/**
+ *
+ * @author Saadate
+ */
+public class Constructible extends CaseAchetable {
+
+    /**
+     * Cet attribut représente le nombre de maisons sur la case
+     */
+    public int maison;
+
+    /**
+     * Cet attribut représente la possibilité de construire un (unique) hôtel sur la case
+     */
+    public boolean hotel;
+
+    public int getMaison() {
+        return maison;
+    }
+
+    public void setMaison(int maison) {
+        this.maison = maison;
+    }
+
+    public boolean isHotel() {
+        return hotel;
+    }
+
+    public void setHotel(boolean hotel) {
+        this.hotel = hotel;
+    }
+
 }
